22b29345111e3c3a72700660bbbefc2c3f042e86
Flávio Medeiros <flavio.medeiros@ifal.edu.br>
2021-07-06 11:05:40 -0300

    Primeira versão do sistema.

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import datetime
+
+TEMPLATES = './templates'
+STATIC = './static'
+
+app = Flask(__name__, template_folder=TEMPLATES, static_folder=STATIC)
+
+@app.route('/')
+def helloWorld():
+    return 'Hello World!'
+
+@app.route('/home')
+def home():
+    data = datetime.datetime.now()
+    usuarios = ['Flávio', 'Isabella', 'João', 'Maria']
+    mostrarUsuarios = True
+    return render_template('home.html', dataAtual=data, usuarios=usuarios, mostrarUsuarios=mostrarUsuarios)
+
+app.run(host='0.0.0.0', port=5000)
